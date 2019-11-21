6442c268062fbb8d53e589f0632f11f26b2ecaa4
vailm <vailm@seas.upenn.edu>
2019-11-21 14:46:23 -0500

    restructuring

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+      </Switch>
+    );
+  }
+}
