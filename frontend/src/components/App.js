b943b469383968f5b8d20f3d924b95c4d6b35455
Karen Shen <shen.karen1@gmail.com>
2019-11-25 15:12:50 -0500

    oops accidentally undid route redirecting

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,10 +14,13 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+        <Switch>
         <Route exact path={ROUTES.LANDING} component={ LandingPage }/>
         <Route exact path={ROUTES.DASHBOARD} component={ Dashboard }/>
         <Route exact path={ROUTES.ROSTER} component={ Roster }/>
         <Route exact path={ROUTES.QUEUE} component={ Queue }/>
+        <Route component={ Dashboard }/>
+        </Switch>
       </Router>
     );
   }
