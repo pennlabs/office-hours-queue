5d2428098953fce88296c57c607ad4814a4b860c
Chris Fischer <chris.fischer36@gmail.com>
2020-01-28 14:11:09 -0500

    Temporary work around for deploying

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,1 +1,6 @@
-(cd "`git rev-parse --show-toplevel`"; git push heroku `git subtree split --prefix backend master`:master --force)
+(cd "`git rev-parse --show-toplevel`";
+ git add -f "backend/ohq-firebase-adminsdk.json";
+ git commit -m "temp";
+ git push heroku `git subtree split --prefix backend master`:master --force;
+ git reset HEAD^;
+ )
