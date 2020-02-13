c13de6d656ffe43f212e8924e5969ba403e7ec7c
Steven Bursztyn <bursztyn@seas.upenn.edu>
2020-02-12 21:02:49 -0500

    lol i dont wanna change analytics rn so Revert "Delete peopledata.js"
    This reverts commit 46b8586b31a67fed0f0b4822e6c7499781f217c5.

---
+++ frontend/src/components/Course/Roster/peopledata.js
@@ -0,0 +1,65 @@
+// take course roster data
+var fakePeople = [
+  {
+      fullName: "Steven Bursztyn",
+      preferredName: "Steven",
+      role: "Head TA",
+      email: "bursztyn@seas.upenn.edu"
+  },
+  {
+      fullName: "Kostas Daniilidis",
+      preferredName: "Kostas",
+      role: "Instructor",
+      email: "kostas@cis.upenn.edu"
+  },
+  {
+      fullName: "Chris Fischer",
+      preferredName: "Chris",
+      role: "TA",
+      email: "cdf@seas.upenn.edu"
+  },
+  {
+      fullName: "Samuel Fleischer",
+      preferredName: "Sam",
+      role: "Student",
+      email: "fls@seas.upenn.edu"
+  },
+  {
+      fullName: "Monal Garg",
+      preferredName: "Monal",
+      role: "TA",
+      email: "mgarg@seas.upenn.edu"
+  },
+  {
+      fullName: "Hyewon Lee",
+      preferredName: "Hyewon",
+      role: "Student",
+      email: "hyewon98@seas.upenn.edu"
+  },
+  {
+      fullName: "Wellington Lee",
+      preferredName: "Welly",
+      role: "Student",
+      email: "wtnlee@seas.upenn.edu"
+  },
+  {
+      fullName: "Chris Murphy",
+      preferredName: "Chris",
+      role: "Instructor",
+      email: "cdmurphy@cis.upenn.edu"
+  },
+  {
+      fullName: "Karen Shen",
+      preferredName: "Karen",
+      role: "Auditor",
+      email: "shenk@seas.upenn.edu"
+  },
+  {
+      fullName: "Marshall Vail",
+      preferredName: "Marshall",
+      role: "TA",
+      email: "vailm@seas.upenn.edu"
+  }
+]
+
+export { fakePeople };
