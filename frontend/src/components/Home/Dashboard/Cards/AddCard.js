3b0ae10ca6f0a102be93fa0a92f8487ff15d0db8
vailm <vailm@seas.upenn.edu>
2020-03-15 16:07:11 -0400

    improve add/join card style

--- frontend/src/components/Home/Dashboard/Cards/AddCard.js
+++ frontend/src/components/Home/Dashboard/Cards/AddCard.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { Segment, Header } from 'semantic-ui-react';
 
-export default class AddCard extends React.Component {
-
-  render() {
-    return (
-      <Segment basic>
-        <Segment inverted color="green"
-          style={{
-            height:"110px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            cursor: "pointer",
-          }}
-          onClick={this.props.clickFunc}>
-          <Header style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}
-            content={ this.props.isStudent ? "Join Course" : "Add Course" }/>
-        </Segment>
-      </Segment>
-    );
-  }
+const cardStyle = {
+  height:"110px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  border: "dashed #21ba45"
 }
+
+const hoverStyle = {
+  height:"110px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  border: "dashed #21ba45",
+  backgroundColor: "#b5f4c4"
+}
+
+const AddCard = (props) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <Segment basic>
+      <Segment style={ hovered ? hoverStyle : cardStyle }
+        onMouseEnter={ () => setHovered(true) }
+        onMouseLeave={ () => setHovered(false) }
+        onClick={ props.clickFunc }>
+        <Header color="green" style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}
+          content={ props.isStudent ? "Join Course" : "Add Course" }/>
+      </Segment>
+    </Segment>
+  )
+}
+
+export default AddCard;
