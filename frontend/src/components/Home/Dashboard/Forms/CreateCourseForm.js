35266b08fbe884a17276ce10a60e3ad9609864c5
vailm <vailm@seas.upenn.edu>
2020-03-15 18:26:16 -0400

    restructuring create course to improve create/cancel buttons

--- frontend/src/components/Home/Dashboard/Forms/CreateCourseForm.js
+++ frontend/src/components/Home/Dashboard/Forms/CreateCourseForm.js
@@ -1,89 +1,43 @@
-import React, { useState } from 'react';
-import { Form, Modal, Button } from 'semantic-ui-react';
-
-import { gql } from 'apollo-boost';
-import { useMutation } from '@apollo/react-hooks';
+import React from 'react';
+import { Form } from 'semantic-ui-react';
 import { semesterOptions } from "../../../../utils/enums";
 
-/* GRAPHQL QUERIES/MUTATIONS */
-const CREATE_COURSE = gql`
-  mutation CreateCourse($input: CreateCourseInput!) {
-    createCourse(input: $input) {
-      course {
-        id
-      }
-      courseUser {
-        id
-      }
-    }
-  }
-`;
-
 const CreateCourseForm = (props) => {
-  /* STATE */
-  const [input, setInput] = useState({ inviteOnly: false });
-
-  /* GRAPHQL QUERIES/MUTATIONS */
-  const [createCourse, { data }] = useMutation(CREATE_COURSE);
-
-  /* HANDLER FUNCTIONS */
-  const handleInputChange = (e, { name, value }) => {
-    input[name] = name === "inviteOnly" ? !input[name] : value;
-    setInput(input);
-  };
-
-  const onSubmit = async () => {
-    if (!input.department || !input.courseCode || !input.courseTitle ||
-        !input.year || !input.semester || !input.courseUserKind) return;
-
-    try {
-      await createCourse({
-        variables: {
-          input: input
-        }
-      });
-      await props.refetch();
-      props.toastFunc(`${input.department} ${input.courseCode}`, true);
-    } catch (e) {
-      props.toastFunc(null, false);
-    } 
-  };
-
   return (
     <Form>
       <Form.Field>
         <label>Department</label>
-        <Form.Input name="department" onChange={ handleInputChange } placeholder="CIS"/>
+        <Form.Input name="department" onChange={ props.changeFunc } placeholder="CIS"/>
       </Form.Field>
       <Form.Field>
         <label>Course Code</label>
-        <Form.Input name="courseCode" onChange={ handleInputChange } placeholder="121"/>
+        <Form.Input name="courseCode" onChange={ props.changeFunc } placeholder="121"/>
       </Form.Field>
       <Form.Field>
         <label>Course Title</label>
-        <Form.Input name="courseTitle" onChange={ handleInputChange } placeholder="Data Structures and Algorithms"/>
+        <Form.Input name="courseTitle" onChange={ props.changeFunc } placeholder="Data Structures and Algorithms"/>
       </Form.Field>
       <Form.Field>
         <label>Description</label>
-        <Form.TextArea name="description" onChange={ handleInputChange } placeholder="(Optional)"/>
+        <Form.TextArea name="description" onChange={ props.changeFunc } placeholder="(Optional)"/>
       </Form.Field>
       <Form.Field>
         <label>Year</label>
-        <Form.Input name="year" onChange={ handleInputChange } placeholder="2020"/>
+        <Form.Input name="year" onChange={ props.changeFunc } placeholder="2020"/>
       </Form.Field>
       <Form.Field>
         <label>Semester</label>
-        <Form.Dropdown name="semester" onChange={ handleInputChange } selection options={ semesterOptions }/>
+        <Form.Dropdown name="semester" onChange={ props.changeFunc } selection options={ semesterOptions }/>
       </Form.Field>
       <Form.Field>
         <label>Invite Only?</label>
-        <Form.Radio name="inviteOnly" onChange={ handleInputChange } value={true} toggle/>
+        <Form.Radio name="inviteOnly" onChange={ props.changeFunc } value={true} toggle/>
       </Form.Field>
       <Form.Field>
         <label>Your Role</label>
         <Form.Dropdown selection
           name="courseUserKind"
-          onChange={ handleInputChange }
+          onChange={ props.changeFunc }
           options={[{
             key: "PROFESSOR",
             value: "PROFESSOR",
@@ -93,9 +47,6 @@ const CreateCourseForm = (props) => {
             value: "HEAD_TA",
             text: "Head TA"
           }]}/>
-      </Form.Field>
-      <Form.Field>
-          <Button content="Create" color = "green" onClick={onSubmit}/>
       </Form.Field>
     </Form>
   );
--- frontend/src/components/Home/Dashboard/Modals/ModalAddInstructorCourse.js
+++ frontend/src/components/Home/Dashboard/Modals/ModalAddInstructorCourse.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'semantic-ui-react';
 import CreateCourseForm from '../Forms/CreateCourseForm';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
 
-export default class ModalAddInstructorCourse extends React.Component {
-  render() {
-    return (
-      <Modal open={ this.props.open }>
-        <Modal.Header>Create New Course</Modal.Header>
-        <Modal.Content>
-          <CreateCourseForm refetch={ this.props.refetch } toastFunc={ this.props.toastFunc }/>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button content="Done" onClick={ this.props.closeFunc }/>
-        </Modal.Actions>
-      </Modal>
-    );
+const CREATE_COURSE = gql`
+  mutation CreateCourse($input: CreateCourseInput!) {
+    createCourse(input: $input) {
+      course {
+        id
+      }
+      courseUser {
+        id
+      }
+    }
   }
+`;
+
+const ModalAddInstructorCourse = (props) => {
+  const [open, setOpen] = useState(props.open);
+  const [input, setInput] = useState({ inviteOnly: false });
+  const [disabled, setDisabled] = useState(true);
+  const [createCourse, { data }] = useMutation(CREATE_COURSE);
+
+  const handleInputChange = (e, { name, value }) => {
+    input[name] = name === "inviteOnly" ? !input[name] : value;
+    setInput(input);
+    setDisabled(!input.department || !input.courseCode || !input.courseTitle ||
+      !input.year || !input.semester || !input.courseUserKind);
+  };
+
+  const onSubmit = async () => {
+    if (!input.department || !input.courseCode || !input.courseTitle ||
+        !input.year || !input.semester || !input.courseUserKind) return;
+
+    try {
+      await createCourse({
+        variables: {
+          input: input
+        }
+      });
+      await props.refetch();
+      props.closeFunc();
+      props.toastFunc(`${input.department} ${input.courseCode}`, true);
+    } catch (e) {
+      props.toastFunc(null, false);
+    } 
+  };
+
+  useEffect(() => {
+    setOpen(props.open);
+  }, [props.open])
+
+  return (
+    <Modal open={ open }>
+      <Modal.Header>Create New Course</Modal.Header>
+      <Modal.Content>
+        <CreateCourseForm changeFunc={ handleInputChange }/>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button content="Cancel" onClick={ props.closeFunc }/>
+        <Button content="Create"
+          color="green" 
+          disabled={ disabled }
+          onClick={ onSubmit }/>
+      </Modal.Actions>
+    </Modal>
+  )
 }
+
+export default ModalAddInstructorCourse;
