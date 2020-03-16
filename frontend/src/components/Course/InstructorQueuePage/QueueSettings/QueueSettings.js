2c78f9a42ab5d42108ee2421f3577156419300c3
vailm <vailm@seas.upenn.edu>
2020-03-16 16:35:46 -0400

    remove from close/open from settings page

--- frontend/src/components/Course/InstructorQueuePage/QueueSettings/QueueSettings.js
+++ frontend/src/components/Course/InstructorQueuePage/QueueSettings/QueueSettings.js
@@ -2,56 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Grid, Segment, Header, Tab, Button } from 'semantic-ui-react';
 import QueueForm from './QueueForm';
 import TagForm from './TagForm';
-import { useMutation } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
-
-const ACTIVATE_QUEUE = gql`
-  mutation ManuallyActivateQueue($input: ManuallyActivateQueueInput!) {
-    manuallyActivateQueue(input: $input) {
-      queue {
-        id
-      }
-    }
-  }
-`;
-
-const DEACTIVATE_QUEUE = gql`
-mutation ManuallyDeactivateQueue($input: ManuallyDeactivateQueueInput!) {
-  manuallyDeactivateQueue(input: $input) {
-    queue {
-      id
-    }
-  }
-}
-`;
 
 const QueueSettings = (props) => {
   /* STATE */
   const [queue, setQueue] = useState(props.queue);
-  const [activateQueue, activateQueueRes] = useMutation(ACTIVATE_QUEUE);
-  const [deactivateQueue, deactivateQueueRes] = useMutation(DEACTIVATE_QUEUE);
-
-  const onOpen = async () => {
-    await activateQueue({
-      variables: {
-        input: {
-          queueId: queue.id
-        }
-      }
-    });
-    props.refetch();
-  }
-
-  const onClose = async () => {
-    await deactivateQueue({
-      variables: {
-        input: {
-          queueId: queue.id
-        }
-      }
-    });
-    props.refetch();
-  }
 
   /* PROPS UPDATE */
   useEffect(() => {
@@ -63,20 +17,7 @@ const QueueSettings = (props) => {
       <Grid.Row>
         <Segment basic>
           <Header as="h3">
-            <Button content="Open"
-              floated="right"
-              color="green"
-              disabled={ queue.activeOverrideTime !== null }
-              onClick={ onOpen }/>
-            <Button content="Close"
-              floated="right"
-              color="red"
-              disabled={ queue.activeOverrideTime === null }
-              onClick={ onClose }/>
-            Queue Settings
-            <Header.Subheader>
-              { queue.name }
-            </Header.Subheader>
+            { queue.name }
           </Header>
         </Segment>
       </Grid.Row>
