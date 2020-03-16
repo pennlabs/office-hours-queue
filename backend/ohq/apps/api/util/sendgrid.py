dd6b52bb6b8a058dbfc1916180cdb1cb7d8f165a
Chris Fischer <chris.fischer36@gmail.com>
2020-03-16 19:02:42 -0400

    Better add email

--- backend/ohq/apps/api/util/sendgrid.py
+++ backend/ohq/apps/api/util/sendgrid.py
@@ -23,8 +23,14 @@ def send_invitation_email(invited_course_user: InvitedCourseUser):
            f"{'an' if invited_course_user.kind == CourseUserKind.ADMIN.name else 'a'} " \
            f"{CourseUserKind.to_pretty(invited_course_user.kind)} by " \
            f"{invited_course_user.invited_by.full_name}."
+    body_p2 = "<p>Click below to create your account!</p>"
 
-    html_content = template.render(title=title, body=body, sign_up_link="https://ohq.io")
+    html_content = template.render(
+        title=title,
+        body=body,
+        body_p2=body_p2,
+        sign_up_link="https://ohq.io",
+    )
     message = Mail(
         from_email=Email("info@ohq.io", "Office Hour Queue"),
         to_emails=invited_course_user.email,
@@ -53,8 +59,14 @@ def send_added_to_course_email(course_user: CourseUser):
            f"{'an' if course_user.kind == CourseUserKind.ADMIN.name else 'a'} " \
            f"{CourseUserKind.to_pretty(course_user.kind)} by " \
            f"{course_user.invited_by.full_name}."
+    body_p2 = ""
 
-    html_content = template.render(title=title, body=body, sign_up_link="https://ohq.io")
+    html_content = template.render(
+        title=title,
+        body=body,
+        body_p2=body_p2,
+        sign_up_link="https://ohq.io",
+    )
     message = Mail(
         from_email=Email("info@ohq.io", "Office Hour Queue"),
         to_emails=course_user.user.email,
