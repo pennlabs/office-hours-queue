1c78486c96dab0468b3b1730eaa912e44cd3cf74
Steven Bursztyn <bursztyn@seas.upenn.edu>
2020-03-16 18:43:31 -0400

    use h2 not h1n1

--- frontend/src/components/Home/AccountSettings/AccountSettings.js
+++ frontend/src/components/Home/AccountSettings/AccountSettings.js
@@ -7,7 +7,7 @@ const AccountSettings = (props) => {
     <Grid.Column width={13}>
         <Grid.Row>
           <Segment basic>
-            <Header as="h1">
+            <Header as="h2">
               Account Settings
             </Header>
           </Segment>
