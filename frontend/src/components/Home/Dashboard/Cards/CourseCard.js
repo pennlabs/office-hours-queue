f7069c459bb39152e893853776b42b36e4e7f743
vailm <vailm@seas.upenn.edu>
2020-03-16 02:32:25 -0400

    make entire card raised on hover

--- frontend/src/components/Home/Dashboard/Cards/CourseCard.js
+++ frontend/src/components/Home/Dashboard/Cards/CourseCard.js
@@ -1,45 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Segment, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../../../constants/routes';
 
 const CourseCard = (props) => {
-  const [hovered, setHovered] = useState(false);
+  const [hover, setHover] = useState(false);
   const path = {
-      pathname: props.kind === 'STUDENT' ? ROUTES.STUDENT : ROUTES.COURSE,
-      state: { courseId: props.id, courseUserId: props.courseUserId }
-    };
-  
+    pathname: props.kind === 'STUDENT' ? ROUTES.STUDENT : ROUTES.COURSE,
+    state: { courseId: props.id, courseUserId: props.courseUserId }
+  };
+
   return (
     <Link to={ path }>
-       <Segment basic
-       onMouseEnter={ () => setHovered(true)}
-       onMouseLeave={ () => setHovered(false)}>
-         {hovered ? <Segment secondary attached="top" color="blue" style={{height:"70px"}}>
-           <Header style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
-             { `${props.department} ${props.courseCode}`}
-             <Header.Subheader style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
-               { props.courseTitle }
-             </Header.Subheader>
-           </Header>
-         </Segment> : 
-         <Segment attached="top" color="blue" style={{height:"70px"}}>
-           <Header style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
-             { `${props.department} ${props.courseCode}`}
-             <Header.Subheader style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
-               { props.courseTitle }
-             </Header.Subheader>
-           </Header>
-         </Segment>}
-
-         <Segment attached="bottom" secondary textAlign="right" style={{height:"40px"}}>
-           <Header as="h6" style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
-             { props.semester } { props.year }
-           </Header>
-         </Segment>
-         </Segment>
-     </Link>
-  )
+      <Segment basic>
+        <Segment.Group raised={ hover }>
+          <Segment attached="top" color="blue" style={{height:"70px"}} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <Header style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
+              { `${props.department} ${props.courseCode}`}
+              <Header.Subheader style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
+                { props.courseTitle }
+              </Header.Subheader>
+            </Header>
+          </Segment>
+          <Segment attached="bottom" secondary textAlign="right" style={{height:"40px"}}>
+            <Header as="h6" style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
+              { props.semester } { props.year }
+            </Header>
+          </Segment>
+        </Segment.Group>
+      </Segment>
+    </Link>
+  );
 }
 
 export default CourseCard;
