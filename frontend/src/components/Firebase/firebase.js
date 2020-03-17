bb983ddb9487010ed30c636f42583e3273944c1a
Steven Bursztyn <bursztyn@seas.upenn.edu>
2020-03-17 15:44:30 -0400

    remove commented out usless code

--- frontend/src/components/Firebase/firebase.js
+++ frontend/src/components/Firebase/firebase.js
@@ -42,31 +42,6 @@ class Firebase {
 
         next(authUser)
       }
-      // if (authUser) {
-      //     this.user(authUser.uid)
-      //         .once('value')
-      //         .then(snapshot => {
-      //             const dbUser = snapshot.val();
-
-      //             // default empty roles
-      //             if (!dbUser.roles) {
-      //                 dbUser.roles = {};
-      //             }
-
-      //             // merge auth and db user
-      //             authUser = {
-      //                 uid: authUser.uid,
-      //                 email: authUser.email,
-      //                 emailVerified: authUser.emailVerified,
-      //                 providerData: authUser.providerData,
-      //                 // ...dbUser,
-      //             };
-
-      //             next(authUser);
-      //         });
-      // } else {
-      //     fallback();
-      // }
     });
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,3 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import './index.css';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import Roster from './Roster';
-import Dashboard from './Dashboard';
-import * as serviceWorker from './serviceWorker';
-
-const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={SignIn} />
-      <Route path="/SignUp" component={SignUp} />
-      <Route path="/SignIn" component={SignIn} />
-      <Route path="/Roster" component={Roster} />
-      <Route path="/Dashboard" component={Dashboard} />
-    </div>
-  </Router>
-)
-
-ReactDOM.render(routing, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
