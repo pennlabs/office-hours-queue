38fab6055377b0b1c1d613deadd0ba0eb8dc5d47
Chris Fischer <chris.fischer36@gmail.com>
2020-03-17 00:23:09 -0400

    Fix blank on tags array

--- backend/ohq/apps/api/models.py
+++ backend/ohq/apps/api/models.py
@@ -301,7 +301,7 @@ class QuestionState(ChoicesEnum):
 class Question(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
-    tags = ArrayField(models.CharField(max_length=20))
+    tags = ArrayField(models.CharField(max_length=20), null=True, blank=True)
     queue = models.ForeignKey(
         Queue,
         related_name='questions',
