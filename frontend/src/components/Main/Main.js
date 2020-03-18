2b893f69255e86266802b244bac716ec23d314a2
vailm <vailm@seas.upenn.edu>
2020-03-18 12:45:43 -0400

    jk chris got it

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -40,8 +40,6 @@ const Main = (props) => {
     }
   }
 
-  if (error) window.location = "/"
-
   return (
     error ? <Redirect to={'/'}/> :
     <Grid columns="equal" divided style={{"width":"100%"}} stackable>
