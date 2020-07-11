64576460a7a8b38df265d314116ef57404616449
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-07-11 18:00:22 -0400

    Use postgres database by default

--- backend/ohq/settings/base.py
+++ backend/ohq/settings/base.py
@@ -83,7 +83,9 @@ WSGI_APPLICATION = "ohq.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3"))
+    "default": dj_database_url.config(
+        default="postgres://postgres:postgres@localhost:5432/postgres"
+    )
 }
 
 
