86200ecf1ff5ca3aa9e289cb47abf9285b27576c
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-07-17 09:50:27 -0400

    Fix CI + lint frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import React from "react";
 
 export default function Home() {
   return (
@@ -54,7 +55,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +206,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
