27f238888f403ce6f8464ad401f6660f87e21b16
Ying Xuan <engyingxuan@gmail.com>
2020-08-05 13:45:03 -0400

    signout button

--- frontend/components/SignOut/index.tsx
+++ frontend/components/SignOut/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import firebase from '../Firebase';
+import React from "react";
+import { useRouter } from "next/router";
+import { Menu, Icon } from "semantic-ui-react";
 
-const SignOutButton = () => (
-  <Menu.Item style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}
-    name="Signout"
-    active={false}
-    onClick={firebase.doSignOut}>
-    <Icon name="sign-out"/>
-    Sign Out
-  </Menu.Item>
-);
+const SignOutButton = () => {
+    const router = useRouter();
+    return (
+        <Menu.Item
+            style={{
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+                overflow: "hidden",
+            }}
+            name="Signout"
+            active={false}
+            onClick={() => {
+                fetch("/api/accounts/logout").then(() => {
+                    router.replace("/");
+                    router.reload();
+                });
+            }}
+        >
+            <Icon name="sign-out" />
+            Sign Out
+        </Menu.Item>
+    );
+};
 
 export default SignOutButton;
