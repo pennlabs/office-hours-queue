c2c451c0d8cc9eed2b4a1b55479805141da6f337
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-08-11 10:32:22 -0400

    Fix course page

--- frontend/pages/courses/[course]/index.tsx
+++ frontend/pages/courses/[course]/index.tsx
@@ -1,20 +1,11 @@
 import { useRouter } from "next/router";
 import Course from "../../../components/Course/Course";
 
-const CoursePage = ({}) => {
+const CoursePage = ({ }) => {
     const router = useRouter();
     const { course } = router.query;
     return (
-        <Course {...course} />
-        // <Grid
-        //     columns={2}
-        //     divided="horizontally"
-        //     style={{ width: "100%" }}
-        //     stackable
-        // >
-        //     <HomeSidebar active={active} clickFunc={setActive} />
-        //     <AccountSettings />
-        // </Grid>
+        <Course courseId={course} />
     );
 };
 
