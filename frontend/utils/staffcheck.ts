65a11f7e31937354c27ed7ad282734575c2130f2
Ying Xuan <engyingxuan@gmail.com>
2020-08-23 09:35:48 -0400

    an oopsie

--- frontend/utils/staffcheck.ts
+++ frontend/utils/staffcheck.ts
@@ -3,17 +3,17 @@ import { User, Kind } from "../types";
 
 export default function staffCheck(user: User, ctx: NextPageContext) {
     const {
-        query: { queryCourse },
+        query: { course },
     } = ctx;
     let parsedCourse: number;
-    if (typeof queryCourse == "string") {
-        parsedCourse = parseInt(queryCourse, 10);
+    if (typeof course == "string") {
+        parsedCourse = parseInt(course, 10);
     } else {
-        parsedCourse = parseInt(queryCourse[0], 10);
+        parsedCourse = parseInt(course[0], 10);
     }
 
-    const course = user.membershipSet.find(
+    const foundCourse = user.membershipSet.find(
         (membership) => membership.course.id === parsedCourse
     );
-    return course.kind !== Kind.STUDENT;
+    return foundCourse.kind !== Kind.STUDENT;
 }
