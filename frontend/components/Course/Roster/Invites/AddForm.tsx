749bcdc8e192a37a28f1604ebbdc37d99bcf97a6
Ying Xuan <engyingxuan@gmail.com>
2020-08-27 10:31:42 -0400

    added placeholder for invite modal

--- frontend/components/Course/Roster/Invites/AddForm.tsx
+++ frontend/components/Course/Roster/Invites/AddForm.tsx
@@ -21,7 +21,11 @@ const AddForm = (props: AddFormProps) => {
                 <label htmlFor="form-email">Email(s)</label>
 
                 {/* TODO: add a validation step here */}
-                <Form.TextArea id="form-email" onChange={onChange} />
+                <Form.TextArea
+                    id="form-email"
+                    onChange={onChange}
+                    placeholder="Separate multiple emails with line breaks or commas"
+                />
             </Form.Field>
             <Form.Field>
                 <label htmlFor="form-role">Role</label>
