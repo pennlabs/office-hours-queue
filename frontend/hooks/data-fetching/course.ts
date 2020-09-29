2d79a10fbdefdfbee9e5cad950448b6ad92eda35
Ying Xuan Eng <engyingxuan@gmail.com>
2020-09-29 11:10:45 -0400

    Undoing websockets (#43)
    * undo websockets again :(
    
    * rescale because no websockets suck
    
    * fix routes
    
    * restore asgi if needed
    
    * fix trailing slash
    
    * the other one

--- frontend/hooks/data-fetching/course.ts
+++ frontend/hooks/data-fetching/course.ts
@@ -124,101 +124,138 @@ export const useQueues = (courseId: number, initialData: Queue[]) =>
         }
     );
 
+// export const useQuestions = (
+//     courseId: number,
+//     queueId: number,
+//     initialData: Question[]
+// ) => {
+//     const { data, ...other } = useRealtimeResourceList(
+//         `/api/courses/${courseId}/queues/${queueId}/questions/`,
+//         (id) => `/api/courses/${courseId}/queues/${queueId}/questions/${id}/`,
+//         {
+//             model: "ohq.Question",
+//             property: "queue_id",
+//             value: queueId,
+//         },
+//         {
+//             initialData,
+//             fetcher: newResourceFetcher,
+//             refreshInterval: STAFF_QUESTION_POLL_INTERVAL,
+//             orderBy: (q1, q2) => {
+//                 const date1 = new Date(q1.timeAsked);
+//                 const date2 = new Date(q2.timeAsked);
+
+//                 if (date1 > date2) {
+//                     return 1;
+//                 } else {
+//                     return -1;
+//                 }
+//             },
+//         }
+//     );
+//     const filteredData = data?.filter(
+//         (q) =>
+//             q.status === QuestionStatus.ACTIVE ||
+//             q.status === QuestionStatus.ASKED
+//     );
+//     return { data: filteredData, ...other };
+// };
+//
+
 export const useQuestions = (
     courseId: number,
     queueId: number,
     initialData: Question[]
-) => {
-    const { data, ...other } = useRealtimeResourceList(
-        `/api/courses/${courseId}/queues/${queueId}/questions`,
+) =>
+    useResourceListNew<Question>(
+        `/api/courses/${courseId}/queues/${queueId}/questions/`,
         (id) => `/api/courses/${courseId}/queues/${queueId}/questions/${id}/`,
         {
-            model: "ohq.Question",
-            property: "queue_id",
-            value: queueId,
-        },
-        {
             initialData,
+            refreshInterval: 5000,
             fetcher: newResourceFetcher,
-            refreshInterval: STAFF_QUESTION_POLL_INTERVAL,
-            orderBy: (q1, q2) => {
-                const date1 = new Date(q1.timeAsked);
-                const date2 = new Date(q2.timeAsked);
-
-                if (date1 > date2) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            },
         }
     );
-    const filteredData = data?.filter(
-        (q) =>
-            q.status === QuestionStatus.ACTIVE ||
-            q.status === QuestionStatus.ASKED
-    );
-    return { data: filteredData, ...other };
-};
+
+// export const useQuestionPosition = (
+//     courseId: number,
+//     queueId: number,
+//     id: number
+// ) => {
+//     const { data: qdata } = useRealtimeResource<Queue>(
+//         `/api/courses/${courseId}/queues/${queueId}/`,
+//         {
+//             model: "ohq.Queue",
+//             property: "id",
+//             value: queueId,
+//         },
+//         { fetcher: newResourceFetcher }
+//     );
+
+//     const [data, error, isValidating, mutate] = useResource(
+//         `/courses/${courseId}/queues/${queueId}/questions/${id}/position/`,
+//         { position: -1 },
+//         { refreshInterval: STUDENT_QUESTION_POS_POLL_INTERVAL }
+//     );
+
+//     const stringified = JSON.stringify(qdata);
+//     useEffect(() => {
+//         mutate();
+//     }, [stringified]);
+//     return [data, error, isValidating, mutate];
+// };
+//
 
 export const useQuestionPosition = (
     courseId: number,
     queueId: number,
     id: number
-) => {
-    const { data: qdata } = useRealtimeResource<Queue>(
-        `/api/courses/${courseId}/queues/${queueId}/`,
-        {
-            model: "ohq.Queue",
-            property: "id",
-            value: queueId,
-        },
-        { fetcher: newResourceFetcher }
-    );
-
-    const [data, error, isValidating, mutate] = useResource(
+) =>
+    useResource(
         `/courses/${courseId}/queues/${queueId}/questions/${id}/position/`,
         { position: -1 },
-        { refreshInterval: STUDENT_QUESTION_POS_POLL_INTERVAL }
+        { refreshInterval: 10000 }
     );
-
-    const stringified = JSON.stringify(qdata);
-    useEffect(() => {
-        mutate();
-    }, [stringified]);
-    return [data, error, isValidating, mutate];
-};
 
 // only student queues should use this, since it doesn't make
 // much sense otherwise
-export const useLastQuestions = (courseId: number, queueId: number) => {
-    const { data: qdata } = useRealtimeResourceList<Question, "queue_id">(
-        `/api/courses/${courseId}/queues/${queueId}/questions`,
-        (id) => `/api/courses/${courseId}/queues/${queueId}/questions/${id}`,
-        {
-            model: "ohq.Question",
-            property: "queue_id",
-            value: queueId,
-        },
-        {
-            fetcher: newResourceFetcher,
-        }
-    );
+// export const useLastQuestions = (courseId: number, queueId: number) => {
+//     const { data: qdata } = useRealtimeResourceList<Question, "queue_id">(
+//         `/api/courses/${courseId}/queues/${queueId}/questions/`,
+//         (id) => `/api/courses/${courseId}/queues/${queueId}/questions/${id}/`,
+//         {
+//             model: "ohq.Question",
+//             property: "queue_id",
+//             value: queueId,
+//         },
+//         {
+//             fetcher: newResourceFetcher,
+//         }
+//     );
 
-    const [data, error, isValidating, mutate] = useResourceList(
+//     const [data, error, isValidating, mutate] = useResourceList(
+//         `/courses/${courseId}/queues/${queueId}/questions/last/`,
+//         (id) => `/courses/${courseId}/queues/${queueId}/last/${id}/`
+//     );
+
+//     const stringified = JSON.stringify(qdata);
+
+//     // this revalidates the last question query whenever there is a websocket update
+//     useEffect(() => {
+//         mutate(-1, null);
+//     }, [stringified]);
+
+//     return [data, error, isValidating, mutate];
+// };
+//
+//
+//
+
+export const useLastQuestions = (courseId: number, queueId: number) =>
+    useResourceList(
         `/courses/${courseId}/queues/${queueId}/questions/last/`,
         (id) => `/courses/${courseId}/queues/${queueId}/last/${id}/`
     );
-
-    const stringified = JSON.stringify(qdata);
-
-    // this revalidates the last question query whenever there is a websocket update
-    useEffect(() => {
-        mutate(-1, null);
-    }, [stringified]);
-
-    return [data, error, isValidating, mutate];
-};
 
 export async function clearQueue(courseId: number, queueId: number) {
     await doApiRequest(`/courses/${courseId}/queues/${queueId}/clear/`, {
