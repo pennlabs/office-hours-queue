e00ca60d078e7c9f5d1497193c16f5fe738e1135
Davis Haupt <davis.haupt@gmail.com>
2020-09-30 17:59:53 -0400

    warning

--- backend/officehoursqueue/routing.py
+++ backend/officehoursqueue/routing.py
@@ -3,6 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 import ohq.routing
+# DO NOT DELETE THIS IMPORT! Django REST Live requires urls to be imported from async entrypoint.
 import ohq.urls
 
 
