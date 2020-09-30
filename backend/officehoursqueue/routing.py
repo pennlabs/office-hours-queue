320c822f0f363f61d0347ba22fb5c8c4326b87cb
Davis Haupt <davis.haupt@gmail.com>
2020-09-30 18:09:43 -0400

    Fix websocket permissions (#45)
    * this is going to be a fun writeup
    
    * warning
    
    * lint

--- backend/officehoursqueue/routing.py
+++ backend/officehoursqueue/routing.py
@@ -3,7 +3,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 import ohq.routing
+import ohq.urls  # DO NOT DELETE THIS IMPORT!
 
+
+# Django REST Live requires urls too be imported from the async entrypoint.
 
 application = ProtocolTypeRouter(
     {
