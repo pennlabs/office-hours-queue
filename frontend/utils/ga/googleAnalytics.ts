ed47c2a648f2fc9017bc09e67fd656b88daa1779
Ying Xuan <engyingxuan@gmail.com>
2020-10-18 12:46:45 -0400

    prevent GA in dev

--- frontend/utils/ga/googleAnalytics.ts
+++ frontend/utils/ga/googleAnalytics.ts
@@ -1,10 +1,16 @@
 import ReactGA from "react-ga";
 
+const prod = process.env.NODE_ENV === "production";
+
 export const initGA = () => {
-    ReactGA.initialize("UA-21029575-18");
+    if (prod) {
+        ReactGA.initialize("UA-21029575-18");
+    }
 };
 
 export const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname);
+    if (prod) {
+        ReactGA.set({ page: window.location.pathname });
+        ReactGA.pageview(window.location.pathname);
+    }
 };
