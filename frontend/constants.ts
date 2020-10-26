378670850ab4faa60c5049896035254eca6d2579
Cons Wang <conswang@seas.upenn.edu>
2020-10-26 17:38:52 -0400

    support non zoom video chat links

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,3 +1,9 @@
 export const QUEUE_STATUS_POLL_INTERVAL = 30000;
 export const STAFF_QUESTION_POLL_INTERVAL = 30000;
 export const STUDENT_QUESTION_POS_POLL_INTERVAL = 10000;
+export const ALLOWED_LINKS = [
+    "zoom.us",
+    "bluejeans.com",
+    "meet.google.com",
+    "whereby.com",
+];
