3e49bafc1396c9647c0e20b46eeadde2a2ee36bf
William Goeller <william@williamgoeller.com>
2020-11-05 23:54:41 -0500

    Fix invalid zoom link issue (#62)
    * Fix invalid zoom link issue
    
    * double quotes
    
    * reverting back
    
    * Add zoom.com.cn
    
    * fix style
    
    * swap out ternary operator

--- frontend/components/Course/StudentQueuePage/QuestionForm.tsx
+++ frontend/components/Course/StudentQueuePage/QuestionForm.tsx
@@ -114,6 +114,10 @@ const QuestionForm = (props: QuestionFormProps) => {
                                 name="videoChatUrl"
                                 placeholder="Sample URL: https://zoom.us/j/123456789?pwd=abcdefg"
                                 onChange={handleInputChange}
+                                error={
+                                    !validURL &&
+                                    "Please enter a valid video link!"
+                                }
                             />
                         </Form.Field>
                     )}
--- frontend/constants.ts
+++ frontend/constants.ts
@@ -3,6 +3,8 @@ export const STAFF_QUESTION_POLL_INTERVAL = 30000;
 export const STUDENT_QUESTION_POS_POLL_INTERVAL = 10000;
 export const ALLOWED_LINKS = [
     "zoom.us",
+    "zoom.com",
+    "zoom.com.cn",
     "bluejeans.com",
     "meet.google.com",
     "whereby.com",
