be4bf2fe82d1e100c7782211913703fb27c4f546
William Goeller <william@williamgoeller.com>
2020-11-08 18:10:37 -0500

    Only show error if link length greater than zero

--- frontend/components/Course/StudentQueuePage/QuestionForm.tsx
+++ frontend/components/Course/StudentQueuePage/QuestionForm.tsx
@@ -115,6 +115,7 @@ const QuestionForm = (props: QuestionFormProps) => {
                                 placeholder="Sample URL: https://zoom.us/j/123456789?pwd=abcdefg"
                                 onChange={handleInputChange}
                                 error={
+                                    input.videoChatUrl.length > 0 &&
                                     !validURL &&
                                     "Please enter a valid video link!"
                                 }
