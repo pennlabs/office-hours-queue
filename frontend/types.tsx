50135004db984548799e4fb6d853da4621deaf54
Cons Wang <conswang@seas.upenn.edu>
2021-02-02 00:26:39 -0500

    update day of week enum

--- frontend/types.tsx
+++ frontend/types.tsx
@@ -193,11 +193,11 @@ export interface HeatmapSeries {
 }
 
 export enum DayOfWeek {
-    "Monday" = 0,
-    "Tuesday" = 1,
-    "Wednesday" = 2,
-    "Thursday" = 3,
-    "Friday" = 4,
-    "Saturday" = 5,
-    "Sunday" = 6,
+    "Monday" = 1,
+    "Tuesday" = 2,
+    "Wednesday" = 3,
+    "Thursday" = 4,
+    "Friday" = 5,
+    "Saturday" = 6,
+    "Sunday" = 7,
 }
