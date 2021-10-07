cac2c8d00ed5d9afb7da584c8c56c1c755520754
William Goeller <william@williamgoeller.com>
2021-10-07 01:02:33 -0400

    Cleanup migration test

--- backend/tests/ohq/test_migrations.py
+++ backend/tests/ohq/test_migrations.py
@@ -7,14 +7,10 @@ from ohq.models import Semester
 
 class MigrationTest(TransactionTestCase):
     """
-    Test applying a new migration.
+    Test applying a new migration on top of an old migration.
         Make sure that `self.migrate_from` and `self.migrate_to` are defined, and also implement
         `setUpBeforeMigration()` to setup the model before migrations are applied.
     """
-
-    @property
-    def app(self):
-        return "ohq"
 
     migrate_from = None  # need to be defined by subclasses
     migrate_to = None  # need to be defined by subclasses
@@ -25,10 +21,10 @@ class MigrationTest(TransactionTestCase):
             self.migrate_to and self.migrate_from
         ), f"TestCase {type(self).__name} must define migrate_to and migrate_from properties"
 
-        self.migrate_from = [(self.app, self.migrate_from)]
-        self.migrate_to = [(self.app, self.migrate_to)]
+        self.migrate_from = [("ohq", self.migrate_from)]
+        self.migrate_to = [("ohq", self.migrate_to)]
         self.executor = MigrationExecutor(connection)
-        self.old_apps = self.executor.loader.project_state(self.migrate_from).apps
+        self.pre_migration = self.executor.loader.project_state(self.migrate_from).apps
 
         # revert to the original migration
         self.executor.migrate(self.migrate_from)
@@ -36,14 +32,19 @@ class MigrationTest(TransactionTestCase):
         # ensure return to the latest migration, even if the test fails
         self.addCleanup(self.force_migrate)
 
-        self.setUpBeforeMigration(self.old_apps)
+        # perform final migration setup
+        self.setUpBeforeMigration(self.pre_migration_app)
+
+        # Finally apply the migration
         self.executor.loader.build_graph()
         self.executor.migrate(self.migrate_to)
-        self.new_apps = self.executor.loader.project_state(self.migrate_to).apps
+        self.post_migration = self.executor.loader.project_state(self.migrate_to).apps
 
+    # Implement in subclasses to setup models before applying a migration
     def setUpBeforeMigration(self, apps):
         pass
 
+    # forces a migration to the latest migration even if tests fail
     def force_migrate(self, migrate_to=None):
         self.executor.loader.build_graph()  # reload.
         if migrate_to is None:
@@ -59,19 +60,19 @@ class TestQuestionTemplatesMigration(MigrationTest):
     migrate_from = "0014_question_student_descriptor"
     migrate_to = "0015_question_templates"
 
-    def setUpBeforeMigration(self, apps):
-        self.semester = self.old_apps.get_model(self.app, "Semester").objects.create(
+    def setUpBeforeMigration(self, pre_migration):
+        self.semester = pre_migration.get_model(self.app, "Semester").objects.create(
             year=2020, term=Semester.TERM_SUMMER
         )
-        self.course = self.old_apps.get_model(self.app, "Course").objects.create(
+        self.course = pre_migration.get_model(self.app, "Course").objects.create(
             course_code="000", department="Penn Labs", semester=self.semester
         )
-        self.queue = self.old_apps.get_model(self.app, "Queue").objects.create(
+        self.queue = pre_migration.get_model(self.app, "Queue").objects.create(
             name="Queue", course=self.course
         )
 
     def test_question_templates_migrated(self):
-        queue = self.new_apps.get_model(self.app, "Queue").objects.get(id=self.queue.id)
+        queue = self.post_migration.get_model(self.app, "Queue").objects.get(id=self.queue.id)
 
         # The queue created before migration now has a question_template attribute
         self.assertIs(hasattr(queue, "question_template"), True)
