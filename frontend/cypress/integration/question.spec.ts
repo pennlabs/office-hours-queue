07c75d1a072063a46f0f0b3c351f03f1587230e2
Gautam Ramesh <gautamshivramesh@gmail.com>
2021-10-08 17:55:23 -0400

    use users from populate script, add zoom link to question

--- frontend/cypress/integration/question.spec.ts
+++ frontend/cypress/integration/question.spec.ts
@@ -1,11 +1,11 @@
 describe("Question Tests", () => {
     beforeEach(() => {
-        cy.login("gautam1", "gautam1");
+        cy.login("tjefferson", "pennlabs");
         cy.getCookie("csrftoken")
             .then((cookie) => cookie.value)
             .then((csrftokenBefore) => {
                 cy.request({
-                    url: "/api/courses/7/queues/9/clear/",
+                    url: "/api/courses/5/queues/6/clear/",
                     method: "POST",
                     headers: {
                         "X-CSRFToken": csrftokenBefore,
@@ -21,7 +21,7 @@ describe("Question Tests", () => {
             100 * Math.random()
         ).toString()}`;
 
-        cy.login("gautam2", "gautam2");
+        cy.login("bfranklin", "pennlabs");
         cy.visit("localhost:3000/");
 
         cy.getCookie("csrftoken").its("value").should("exist");
@@ -34,7 +34,7 @@ describe("Question Tests", () => {
                     .then((sessionidStudent) => {
                         cy.clearCookies();
 
-                        cy.login("gautam1", "gautam1");
+                        cy.login("tjefferson", "pennlabs");
                         cy.visit("localhost:3000/");
 
                         cy.getCookie("csrftoken")
@@ -43,7 +43,7 @@ describe("Question Tests", () => {
                                 cy.getCookie("sessionid")
                                     .then((cookie) => cookie.value)
                                     .then((sessionidTA) => {
-                                        cy.contains("cis cis101").click();
+                                        cy.contains("LABS 621").click();
                                         cy.wait(3000);
 
                                         cy.clearCookies();
@@ -51,7 +51,7 @@ describe("Question Tests", () => {
                                         cy.request({
                                             failOnStatusCode: true,
                                             url:
-                                                "/api/courses/7/queues/9/questions/",
+                                                "/api/courses/5/queues/6/questions/",
                                             method: "POST",
                                             headers: {
                                                 cookie: `csrftoken=${csrftokenStudent}; sessionid=${sessionidStudent}`,
@@ -61,10 +61,11 @@ describe("Question Tests", () => {
                                                 text: questionString,
                                                 tags: [],
                                                 askedBy: {
-                                                    email: "b@b.com",
-                                                    firstName: "",
-                                                    lastName: "",
-                                                    username: "gautam2",
+                                                    email:
+                                                        "bfranklin@seas.upenn.edu",
+                                                    firstName: "Benjamin",
+                                                    lastName: "Franklin",
+                                                    username: "bfranklin",
                                                 },
                                             },
                                         });
@@ -95,18 +96,19 @@ describe("Question Tests", () => {
             100 * Math.random()
         ).toString()}`;
 
-        cy.login("gautam2", "gautam2");
+        cy.login("bfranklin", "pennlabs");
         cy.visit("localhost:3000/");
-        cy.contains("cis cis101").click();
+        cy.contains("LABS 621").click();
         cy.get("textarea").type(questionString);
+        cy.get("input").type("https://upenn.zoom.us/j/6017796013");
 
         cy.contains("Submit").click();
 
         cy.logout();
-        cy.login("gautam1", "gautam1");
+        cy.login("tjefferson", "pennlabs");
         cy.visit("localhost:3000/");
 
-        cy.contains("cis cis101").click();
+        cy.contains("LABS 621").click();
         cy.contains(questionString, { timeout: 5000 }).should("be.visible");
 
         cy.contains("Reject").click();
