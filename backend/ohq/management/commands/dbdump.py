e4796c40902fb60398dc3756cdf8324c736da35d
bqle <benjaminle2002@gmail.com>
2022-02-11 18:16:17 -0500

    dump all classes to csv

--- backend/ohq/management/commands/dbdump.py
+++ backend/ohq/management/commands/dbdump.py
@@ -3,18 +3,44 @@ from django.core import serializers
 import inspect
 from ohq.invite import parse_and_send_invites
 import ohq.models
+import json
 
 
-def dumpClass(cls, cls_name, file):
+def dumpClass(cls, cls_name):
+    file = open(cls_name +'.csv', 'w+')
+
     if inspect.isclass(cls):
         try :
+            field_names = [field.name for field in cls._meta.get_fields()]
+
+            for i, name in enumerate(field_names):
+                if name == 'id':
+                    field_names[i] = 'pk'
+
+            while(field_names[0] != 'pk'):
+                field_names.pop(0)
+
+            file.write(str(field_names)[1:-1] + "\n")
+
             all_objs = cls.objects.all()
             for obj in all_objs: 
-                serialized_obj = serializers.serialize('json', [obj, ])
+                obj_placeholder = serializers.serialize('json', [obj, ])
+                obj_parsed = json.loads(obj_placeholder)[0]
+                obj_placeholder = []
+
+                for field_name in field_names:
+                    if field_name in obj_parsed:
+                        obj_placeholder.append(obj_parsed[field_name])
+                    elif field_name in obj_parsed['fields']:
+                        obj_placeholder.append(obj_parsed['fields'][field_name])
+                    else:
+                        obj_placeholder.append(None)
+            
                 try:
-                    file.write(serialized_obj +"\n")
+                    file.write(json.dumps(obj_placeholder)[1:-1] + "\n")
+                    
                 except Exception:
-                    print('error writing object', serialized_obj)
+                    print('error writing object', cls_name)
 
         except Exception:
             print('error getting objects: ', cls_name)
@@ -32,10 +58,13 @@ class Command(BaseCommand):
         if (kwargs['class'] is None):
             # dump every class
             for (name, cls) in inspect.getmembers(ohq.models):
-                dumpClass(cls, name, file)
+                if (name[0:2] != '__') and (name != 'User') and (name[0].isupper()):
+                    print("whent here")
+                    dumpClass(cls, name)
             file.close()
         else:
             # Usage: dbdump --class Course Semester Announcements ... 
+            
             for (name, cls) in inspect.getmembers(ohq.models):
                 if name in kwargs['class']:
                     dumpClass(cls, name, file)
