99f196bf1f506ef3092c9b30a806864e188dc84e
Joy Liu <joyliu.q@gmail.com>
2022-04-01 19:44:18 -0400

    :bug: Switch over to DOMAINS env variable

--- backend/officehoursqueue/settings/base.py
+++ backend/officehoursqueue/settings/base.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 
-DOMAIN = os.environ.get("DOMAIN", "example.com")
+DOMAIN = os.environ.get("DOMAINS", "example.com")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
