bb8137e6ab4b4f906ae9f9d992f82722ea4b2a78
bqle <benjaminle2002@gmail.com>
2022-10-16 16:06:54 -0400

    transition to boto3

--- backend/officehoursqueue/settings/base.py
+++ backend/officehoursqueue/settings/base.py
@@ -192,6 +192,6 @@ CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
 
 # Upload file storage
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
+AWS_STORAGE_BUCKET_NAME = "ohq.question_files"
