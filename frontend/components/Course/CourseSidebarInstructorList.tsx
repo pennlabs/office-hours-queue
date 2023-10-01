035b7bbaf8c4ba835600eac3a9c185dcdbb8de93
charisgao <charis.m.gao@gmail.com>
2023-10-01 14:26:57 -0400

    update mobile

--- frontend/components/Course/CourseSidebarInstructorList.tsx
+++ frontend/components/Course/CourseSidebarInstructorList.tsx
@@ -1,8 +1,11 @@
 import { Header, Icon, List, Segment } from "semantic-ui-react";
+import { useMediaQuery } from "@material-ui/core";
 import { useLeadership } from "../../hooks/data-fetching/course";
 import { Membership } from "../../types";
 import { leadershipSortFunc } from "../../utils";
 import { prettifyRole } from "../../utils/enums";
+import { MOBILE_BP } from "../../constants";
+import Footer from "../common/Footer";
 
 interface CourseSidebarInstructorListProps {
     courseId: number;
@@ -18,35 +21,39 @@ const CourseSidebarInstructorList = ({
         leadershipRaw
     );
     const leadership = leadershipUnsorted.sort(leadershipSortFunc);
+    const isMobile = useMediaQuery(`(max-width: ${MOBILE_BP})`);
 
     return (
-        <Segment basic>
-            <Header as="h3">Instructors</Header>
-            <List>
-                {leadership.map((membership) => {
-                    return (
-                        <List.Item
-                            key={membership.id}
-                            style={{ marginBottom: "8px" }}
-                        >
-                            <Icon name="user" />
-                            <List.Content>
-                                <List.Header
-                                    as="a"
-                                    target="_blank"
-                                    href={`mailto:${membership.user.email}`}
-                                >
-                                    {`${membership.user.firstName} ${membership.user.lastName}`}
-                                </List.Header>
-                                <List.Description>
-                                    {prettifyRole(membership.kind)}
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                    );
-                })}
-            </List>
-        </Segment>
+        <>
+            <Segment basic>
+                <Header as="h3">Instructors</Header>
+                <List>
+                    {leadership.map((membership) => {
+                        return (
+                            <List.Item
+                                key={membership.id}
+                                style={{ marginBottom: "8px" }}
+                            >
+                                <Icon name="user" />
+                                <List.Content>
+                                    <List.Header
+                                        as="a"
+                                        target="_blank"
+                                        href={`mailto:${membership.user.email}`}
+                                    >
+                                        {`${membership.user.firstName} ${membership.user.lastName}`}
+                                    </List.Header>
+                                    <List.Description>
+                                        {prettifyRole(membership.kind)}
+                                    </List.Description>
+                                </List.Content>
+                            </List.Item>
+                        );
+                    })}
+                </List>
+            </Segment>
+            {isMobile && <Footer />}
+        </>
     );
 };
 export default CourseSidebarInstructorList;
