ffaae2ad9433fc645ef19f1a40352e50744f2805
Eunsoo Shin (esinx) <me@esinx.net>
2023-10-14 16:16:20 -0400

    feat(banner):maintenance

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "../styles/index.css";
 import { SWRConfig } from "swr";
+// eslint-disable-next-line import/no-extraneous-dependencies -- weird, but eslint doesn't like this
+import { Announcement } from "@esinx/pennlabs-ui";
 import { doApiRequest } from "../utils/fetch";
 import withGA from "../utils/ga/withGA";
 import { askNotificationPermissions } from "../utils/notifications";
@@ -11,17 +13,30 @@ const MyApp = ({ Component, pageProps }) => {
         askNotificationPermissions();
     }, []);
     return (
-        <SWRConfig
-            value={{
-                fetcher: (path, ...args) =>
-                    doApiRequest(path, ...args).then((res) => res.json()),
-                refreshWhenHidden: true,
-            }}
-        >
-            {/* This is necessary for generic HOC */}
-            {/* eslint-disable-next-line */}
-            <Component {...pageProps} />
-        </SWRConfig>
+        <>
+            <SWRConfig
+                value={{
+                    fetcher: (path, ...args) =>
+                        doApiRequest(path, ...args).then((res) => res.json()),
+                    refreshWhenHidden: true,
+                }}
+            >
+                {/* This is necessary for generic HOC */}
+                {/* eslint-disable-next-line */}
+                <Component {...pageProps} />
+            </SWRConfig>
+            <div
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    padding: 20,
+                }}
+            >
+                <Announcement title="Weekend Maintenance Alert" type="issue" />
+            </div>
+        </>
     );
 };
 
