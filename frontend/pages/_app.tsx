c74b74e9ef4e32516d20f2e2b4841a0a6f3a9bd3
Eunsoo Shin <me@esinx.net>
2023-10-22 14:02:19 -0400

    fix: remove banner

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,6 @@ import "semantic-ui-css/semantic.min.css";
 import "../styles/index.css";
 
 import { useEffect } from "react";
-import { Announcement } from "@esinx/pennlabs-ui";
 import { SWRConfig } from "swr";
 
 import { doApiRequest } from "../utils/fetch";
@@ -14,30 +13,17 @@ const MyApp = ({ Component, pageProps }) => {
         askNotificationPermissions();
     }, []);
     return (
-        <>
-            <SWRConfig
-                value={{
-                    fetcher: (path, ...args) =>
-                        doApiRequest(path, ...args).then((res) => res.json()),
-                    refreshWhenHidden: true,
-                }}
-            >
-                {/* This is necessary for generic HOC */}
-                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-                <Component {...pageProps} />
-            </SWRConfig>
-            <div
-                style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    padding: 20,
-                }}
-            >
-                <Announcement title="Weekend Maintenance Alert" type="issue" />
-            </div>
-        </>
+        <SWRConfig
+            value={{
+                fetcher: (path, ...args) =>
+                    doApiRequest(path, ...args).then((res) => res.json()),
+                refreshWhenHidden: true,
+            }}
+        >
+            {/* This is necessary for generic HOC */}
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <Component {...pageProps} />
+        </SWRConfig>
     );
 };
 
