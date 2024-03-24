986eb2ea2c4b5670fc196357da245c001cbe756b
benjmnxu <benxu@seas.upenn.edu>
2024-03-24 00:52:13 -0400

    static file test

--- backend/officehoursqueue/settings/base.py
+++ backend/officehoursqueue/settings/base.py
@@ -196,4 +196,4 @@ AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
