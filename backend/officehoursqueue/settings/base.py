21d9e3144fe3ade18913e1a9d123c7e1998c171f
benjmnxu <benxu@seas.upenn.edu>
2024-03-24 01:09:50 -0400

    modify installed apps

--- backend/officehoursqueue/settings/base.py
+++ backend/officehoursqueue/settings/base.py
@@ -50,8 +50,7 @@ INSTALLED_APPS = [
     "email_tools.apps.EmailToolsConfig",
     "accounts.apps.AccountsConfig",
     "ohq.apps.OhqConfig",
-    "schedule",
-    "storages"
+    "schedule"
 ]
 
 MIDDLEWARE = [
@@ -195,5 +194,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
