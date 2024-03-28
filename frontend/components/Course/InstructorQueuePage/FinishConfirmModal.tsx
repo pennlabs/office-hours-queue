21a77c5e39b0808910982a99bed91bcd2a0a1f32
Daniel Xue <daniel.l.xue@gmail.com>
2024-03-27 21:42:19 -0400

    implementation of finish question as non-answerer

---
+++ frontend/components/Course/InstructorQueuePage/FinishConfirmModal.tsx
@@ -0,0 +1,34 @@
+import { Button, Modal } from "semantic-ui-react";
+
+interface FinishConfirmModalProps {
+    onFinish: (forced?: boolean) => void;
+    onClose: () => void;
+    open: boolean;
+}
+
+const FinishConfirmModal = (props: FinishConfirmModalProps) => {
+    const { onFinish, onClose, open } = props;
+
+    return (
+        <Modal open={open}>
+            <Modal.Header>Finish Confirmation</Modal.Header>
+            <Modal.Content>
+                <Modal.Description>
+                    You are about to finish a question that you did not start.
+                    Is this intended?
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button content="Cancel" onClick={onClose} />
+                <Button
+                    content="Finish"
+                    loading={false}
+                    color="green"
+                    onClick={() => onFinish(true)}
+                />
+            </Modal.Actions>
+        </Modal>
+    );
+};
+
+export default FinishConfirmModal;
