eef77ef2f593db772c53d4d1b2978b119bef83ae
clay53 <clayton@claytondoesthings.xyz>
2025-03-04 17:45:23 -0500

    Comments for proddev changes

--- backend/officehoursqueue/settings/proddev.py
+++ backend/officehoursqueue/settings/proddev.py
@@ -1,13 +1,19 @@
+# Django config based off of production config, but with minor changes to ensure it works on dev machines
+
 from officehoursqueue.settings.production import *
 
 import officehoursqueue.settings.base as base
 
+# Turning debug logs shouldn't introduce major differences, but will help with debugging
 DEBUG = True
 
+# No https on dev machines
 SECURE_PROXY_SSL_HEADER = ()
 
+# Prevents request rejection on dev machines
 ALLOWED_HOSTS = ["*"]
 
+# Use local login instead of UPenn's
 PLATFORM_ACCOUNTS = base.PLATFORM_ACCOUNTS
 
 # Allow http callback for DLA
