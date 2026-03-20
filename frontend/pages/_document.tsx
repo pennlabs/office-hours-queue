dde7acbbc7e984a32907f04ee2d20be2a2b31f03
June Guo <juneguo257@gmail.com>
2026-03-20 14:44:58 -0400

    linter fix v2

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -14,10 +14,7 @@ const Document = () => {
             <body>
                 <Main />
                 <NextScript />
-                <script
-                    src="https://status.pennlabs.org/banner.js"
-                    defer
-                />
+                <script src="https://status.pennlabs.org/banner.js" defer />
             </body>
         </Html>
     );
